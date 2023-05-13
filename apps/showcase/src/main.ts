8a43803b60f0b74480f834544d68fe0f8a92b383
tmac12 <mba121@gmail.com>
2023-05-13 15:30:12 +0200

    chore: update angular and nx dependencies

--- apps/showcase/src/main.ts
+++ apps/showcase/src/main.ts
@@ -1,11 +1,8 @@
-import { bootstrapApplication } from "@angular/platform-browser";
-import {
-  provideRouter,
-  withEnabledBlockingInitialNavigation,
-} from "@angular/router";
-import { appRoutes } from "./app/app.routes";
-import { AppComponent } from "./app/app.component";
+import { appConfig } from './app/app.config';
+import { bootstrapApplication } from '@angular/platform-browser';
 
-bootstrapApplication(AppComponent, {
-  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation())],
-}).catch((err) => console.error(err));
+import { AppComponent } from './app/app.component';
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
