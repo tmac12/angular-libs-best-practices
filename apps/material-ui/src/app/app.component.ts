3da5a0af11cac483829a76b361b099c521f8a992
Bortolazzo Marco <bortolazzo.marco@breton.it>
2023-05-19 17:01:46 +0200

    feat: add dashboard

--- apps/material-ui/src/app/app.component.ts
+++ apps/material-ui/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { ChipsComponent } from './chips/chips.component';
+import { DashComponent } from './dash/dash.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, ChipsComponent],
+  imports: [NxWelcomeComponent, ChipsComponent, DashComponent],
   selector: 'angular-libs-best-practices-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
