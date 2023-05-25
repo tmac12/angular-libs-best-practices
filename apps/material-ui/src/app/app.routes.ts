9c42f6d6c34bead0ad325e0b4a8fb3f548a73169
Bortolazzo Marco <bortolazzo.marco@breton.it>
2023-05-25 18:12:31 +0200

    feat: add navigation

--- apps/material-ui/src/app/app.routes.ts
+++ apps/material-ui/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Route } from '@angular/router';
 import { ChipsComponent } from './chips/chips.component';
 import { HomeComponent } from './home/home.component';
 import { DashComponent } from './dash/dash.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 export const appRoutes: Route[] = [
   { path: 'home', component: HomeComponent },
   { path: 'chips', component: ChipsComponent },
   { path: 'dash', component: DashComponent },
+  { path: 'nav', component: NavigationComponent },
   { path: '**', redirectTo: 'home' },
 ];
---
+++ apps/material-ui/src/app/navigation/navigation.component.ts
@@ -0,0 +1,36 @@
+import { Component, inject, ChangeDetectionStrategy } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'angular-libs-best-practices-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    CommonModule,
+  ],
+})
+export class NavigationComponent {
+  private breakpointObserver = inject(BreakpointObserver);
+
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map((result) => result.matches),
+      shareReplay()
+    );
+}
