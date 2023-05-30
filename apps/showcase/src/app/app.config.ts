4761a7bc08ee62687e203ce41c83a9325f66cba7
Bortolazzo Marco <bortolazzo.marco@breton.it>
2023-05-30 15:42:35 +0200

    feat: add toastr npm lib

--- apps/showcase/src/app/app.config.ts
+++ apps/showcase/src/app/app.config.ts
@@ -4,6 +4,16 @@ import {
   withEnabledBlockingInitialNavigation,
 } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+import { provideToastr } from 'ngx-toastr';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation())],
+  providers: [
+    provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
+    provideHttpClient(),
+    provideAnimations(), // required animations providers
+    provideToastr(), // Toastr providers
+  ],
 };
